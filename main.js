96883320ef01845967e5d66ba70dd1b39ed48214
Alejandro Barrero <krote95@gmail.com>
2025-02-09 20:00:03 +0100

    Proyecto DOM

---
+++ main.js
@@ -0,0 +1,82 @@
+const products = [
+    {
+        name: 'HP Essentials 255 G8 AMD',
+        price: 289,
+        stars: 4,
+        reviews: 250,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+    },
+    {
+        name: 'Apple MacBook Air M1',
+        price: 999,
+        stars: 5,
+        reviews: 1500,
+        seller: 'Apple Store',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1056/10569477/129-apple-macbook-air-apple-m1-8gb-512gb-ssd-gpu-hepta-core-133-gris-espacial.jpg'
+    },
+    {
+        name: 'Lenovo ThinkPad X1',
+        price: 1249,
+        stars: 4.5,
+        reviews: 320,
+        seller: 'Lenovo',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1063/10637970/1784-lenovo-thinkpad-x1-carbon-gen-10-intel-core-i5-1240p-16gb-512gb-ssd-14.jpg'
+    },
+    {
+        name: 'Dell Inspiron 3530',
+        price: 699,
+        stars: 4,
+        reviews: 500,
+        seller: 'Dell',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1086/10861013/133-dell-inspiron-3530-intel-core-i7-1355u-16gb-1tb-ssd-156.jpg'
+    },
+    {
+        name: 'Asus ROG Zephyrus',
+        price: 1499,
+        stars: 4.8,
+        reviews: 210,
+        seller: 'Asus',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1081/10815884/1338-asus-rog-zephyrus-g14-oled-ga403ui-qs049-amd-ryzen-9-8945hs-32gb-1tb-rtx-4070-14.jpg'
+    },
+    {
+        name: 'Acer Aspire 5',
+        price: 549,
+        stars: 3.5,
+        reviews: 80,
+        seller: 'Acer',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1081/10814405/157-acer-aspire-5-a515-57-57hq-intel-core-i5-12450h-16gb-512gb-ssd-156-teclado-italiano-opiniones.jpg'
+    },
+    {
+        name: 'Microsoft Surface Laptop 7',
+        price: 1099,
+        stars: 4.7,
+        reviews: 900,
+        seller: 'Microsoft Store',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1084/10848679/1993-microsoft-surface-laptop-7-negro-qualcomm-snapdragon-x-elite-x1e-80-100-16gb-512gb-ssd-15-tactil.jpg'
+    },
+    {
+        name: 'Samsung Galaxy Book Pro 4',
+        price: 899,
+        stars: 4.2,
+        reviews: 450,
+        seller: 'Samsung',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1083/10832869/1749-samsung-galaxy-book-4-pro-360-intel-core-ultra-7-155h-16gb-512gb-ssd-16-tactil-fr.jpg'
+    },
+    {
+        name: 'Razer Blade 15',
+        price: 1799,
+        stars: 4.9,
+        reviews: 600,
+        seller: 'Razer',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1037/10377614/1129-razer-blade-15-advanced-model-qhd-intel-core-i7-11800h-16-gb-1tb-ssd-rtx-3070-156.jpg'
+    },
+    {
+        name: 'HP Pavilion 15',
+        price: 799,
+        stars: 4.3,
+        reviews: 300,
+        seller: 'HP Store',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1073/10733273/1864-hp-pavilion-15-eg3014ns-intel-core-i7-1355u-16gb-512gb-ssd-156.jpg'
+    }
+];
